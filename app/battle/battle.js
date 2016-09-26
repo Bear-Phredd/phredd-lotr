1c5e29d2b404fdb1fecb32a53f488a758e734f29
Frédéric Mazaudier <frederic@bear2b.com>
2016-09-26 09:26:07 +0200

    start fighting

--- app/battle/battle.js
+++ app/battle/battle.js
@@ -23,107 +23,11 @@ function battleController($compile)
 	{
 		size	:
 		{
-			x	: 18,
-			y	: 12,
+			x	: 5,
+			y	: 5,
 		},
 		object	:
 		[
-			{
-				type	: 'rock1',
-				cost	: 0,
-				x		: 2,
-				y		: 3,
-			},
-			{
-				type	: 'rock1',
-				cost	: 0,
-				x		: 5,
-				y		: 9,
-			},
-			{
-				type	: 'rock1',
-				cost	: 0,
-				x		: 1,
-				y		: 1,
-			},
-			{
-				type	: 'rock1',
-				cost	: 0,
-				x		: 13,
-				y		: 11,
-			},
-			{
-				type	: 'rock1',
-				cost	: 0,
-				x		: 3,
-				y		: 3,
-			},
-			{
-				type	: 'rock1',
-				cost	: 0,
-				x		: 13,
-				y		: 3,
-			},
-			{
-				type	: 'tree1',
-				cost	: 3,
-				x		: 3,
-				y		: 4,
-			},
-			{
-				type	: 'tree1',
-				cost	: 3,
-				x		: 7,
-				y		: 11,
-			},
-			{
-				type	: 'tree2',
-				cost	: 3,
-				x		: 8,
-				y		: 11,
-			},
-			{
-				type	: 'tree3',
-				cost	: 3,
-				x		: 9,
-				y		: 11,
-			},
-			{
-				type	: 'tree4',
-				cost	: 3,
-				x		: 6,
-				y		: 11,
-			},
-			{
-				type	: 'tree5',
-				cost	: 3,
-				x		: 5,
-				y		: 11,
-			},
-			{
-				type	: 'tree6',
-				cost	: 3,
-				x		: 4,
-				y		: 11,
-			},
-			{
-				type	: 'tree3',
-				cost	: 3,
-				x		: 7,
-				y		: 10,
-			},
-			{
-				type	: 'ruin1',
-				cost	: 2,
-				x		: 9,
-				y		: 5,
-			},
-			{
-				type	: 'ruin2',
-				cost	: 2,
-				x		: 8,
-				y		: 7,
-			},
 		],
 		army	:
 		[
@@ -142,175 +46,25 @@ function battleController($compile)
 				img 	: 6,
 				x		: 1,
 				y		: 4,
-				move	: 4
-			},
-			{
-				team	: 0,
-				name 	: "orc2",
-				img 	: 6,
-				x		: 1,
-				y		: 5,
-				move	: 4
-			},
-			{
-				team	: 0,
-				name 	: "orc3",
-				img 	: 6,
-				x		: 1,
-				y		: 6,
-				move	: 4
-			},
-			{
-				team	: 0,
-				name 	: "orc4",
-				img 	: 6,
-				x		: 1,
-				y		: 7,
-				move	: 4
-			},
-			{
-				team	: 0,
-				name 	: "orc5",
-				img 	: 6,
-				x		: 1,
-				y		: 8,
-				move	: 4
-			},
-			{
-				team	: 0,
-				name 	: "gob1",
-				img 	: 7,
-				x		: 2,
-				y		: 4,
-				move	: 5
-			},
-			{
-				team	: 0,
-				name 	: "gob2",
-				img 	: 7,
-				x		: 2,
-				y		: 5,
-				move	: 5
-			},
-			{
-				team	: 0,
-				name 	: "gob3",
-				img 	: 7,
-				x		: 2,
-				y		: 6,
-				move	: 5
-			},
-			{
-				team	: 0,
-				name 	: "gob4",
-				img 	: 7,
-				x		: 2,
-				y		: 7,
-				move	: 5
-			},
-			{
-				team	: 0,
-				name 	: "gob5",
-				img 	: 7,
-				x		: 2,
-				y		: 8,
-				move	: 5
-			},
-			{
-				team	: 0,
-				name 	: "Ren l'Impur",
-				img 	: 10,
-				x		: 3,
-				y		: 6,
-				move	: 7
-			},
-			{
-				team	: 0,
-				name 	: "Troll1",
-				img 	: 8,
-				x		: 3,
-				y		: 5,
-				move	: 2
-			},
-			{
-				team	: 0,
-				name 	: "Troll2",
-				img 	: 8,
-				x		: 3,
-				y		: 7,
-				move	: 3
-			},
-			{
-				team	: 1,
-				name 	: "Gimli",
-				img 	: 4,
-				x		: 13,
-				y		: 8,
-				move	: 5
-			},
-			{
-				team	: 1,
-				name 	: "Frodo",
-				img 	: 1,
-				x		: 14,
-				y		: 4,
-				move	: 6
-			},
-			{
-				team	: 1,
-				name 	: "Sam",
-				img 	: 1,
-				x		: 14,
-				y		: 5,
-				move	: 6
-			},
-			{
-				team	: 1,
-				name 	: "Merry",
-				img 	: 1,
-				x		: 14,
-				y		: 6,
-				move	: 6
-			},
-			{
-				team	: 1,
-				name 	: "Pippin",
-				img 	: 1,
-				x		: 14,
-				y		: 7,
-				move	: 6
-			},
-			{
-				team	: 1,
-				name 	: "Aragorn",
-				img 	: 2,
-				x		: 13,
-				y		: 5,
-				move	: 5
-			},
-			{
-				team	: 1,
-				name 	: "Boromir",
-				img 	: 2,
-				x		: 13,
-				y		: 6,
-				move	: 5
-			},
-			{
-				team	: 1,
-				name 	: "Gandalf",
-				img 	: 9,
-				x		: 13,
-				y		: 4,
+				att		: 60,
+				def		: 30,
+				pv		: 10,
+				armor	: 3,
+				damage	: 5,
 				move	: 5
 			},
 			{
 				team	: 1,
 				name 	: "Legolas",
 				img 	: 5,
-				x		: 13,
-				y		: 7,
-				move	: 6
+				x		: 5,
+				y		: 5,
+				att		: 70,
+				def		: 50,
+				pv		: 5,
+				armor	: 4,
+				damage	: 5,
+				move	: 3
 			},
 		],
 	};
@@ -330,52 +84,40 @@ function battleController($compile)
 	}
 	oMap.size.arrayY	= aY;
 
-	var aReachable	= [];
-	var aAttackable	= [];
+	var aSearchedCells		= [];
+	var aAttackableCells	= [];
 
 	function getNeighbour(iX, iY, iMove)
 	{
-		if(oConfig.bDebug)
-			console.info('getNeighbour(iX='+iX+', iY='+iY+', iMove='+iMove+')');
-
 		getEastNeighbour(iX, iY, iMove);
 		getWestNeighbour(iX, iY, iMove);
 		getSouthNeighbour(iX, iY, iMove);
 		getNorthNeighbour(iX, iY, iMove);
 
-		aReachable	= [];
-		aAttackable	= [];
+		aSearchedCells	= [];
 	}
 
 	function getEastNeighbour(iX, iY, iMove)
 	{
-		if(oConfig.bDebug)
-			console.info('getEastNeighbour(iX='+iX+', iY='+iY+', iMove='+iMove+')');
-
 		iX++;
 
-		if (!isFoundNeighbour(iX, iY) && iX<oMap.size.x)
+		if (iX<=oMap.size.x)
 			handleNeighbour(iX, iY, iMove);
 	}
 
 	function getWestNeighbour(iX, iY, iMove)
 	{
-		if(oConfig.bDebug)
-			console.info('getWestNeighbour(iX='+iX+', iY='+iY+', iMove='+iMove+')');
-
 		iX--;
 
-		if (!isFoundNeighbour(iX, iY) && iX>0)
+		if (iX>0)
 			handleNeighbour(iX, iY, iMove);
 	}
 
 	function getSouthNeighbour(iX, iY, iMove)
 	{
-		if(oConfig.bDebug)
-			console.info('getSouthNeighbour(iX='+iX+', iY='+iY+', iMove='+iMove+')');
-
 		iY++;
-		if (!isFoundNeighbour(iX, iY) && iY<oMap.size.y)
+
+		if (iY<=oMap.size.y)
 		{
 			handleNeighbour(iX, iY, iMove);
 		}
@@ -383,21 +125,17 @@ function battleController($compile)
 
 	function getNorthNeighbour(iX, iY, iMove)
 	{
-		if(oConfig.bDebug)
-			console.info('getNorthNeighbour(iX='+iX+', iY='+iY+', iMove='+iMove+')');
-
 		iY--;
 
-		if (!isFoundNeighbour(iX, iY) && iY>0)
+		if (iY>0)
 			handleNeighbour(iX, iY, iMove);
 	}
 
 	function isFoundNeighbour(iX, iY)
 	{
-		var iPos1	= aReachable.indexOf(iX + '/' + iY);
-		var iPos2	= aAttackable.indexOf(iX + '/' + iY);
+		var iPos	= aSearchedCells.indexOf(iX + '/' + iY);
 
-		if(iPos1 === -1 && iPos2 === -1)
+		if(iPos === -1)
 			return false;
 
 		return true;
@@ -405,26 +143,29 @@ function battleController($compile)
 
 	function addReachable(iX, iY, dCell)
 	{
-		aReachable.push(iX + '/' + iY);
+		aSearchedCells.push(iX + '/' + iY);
 		angular.element(dCell).addClass('reachable');
 	}
 
 	function addAttackable(iX, iY, dCell)
 	{
-		aAttackable.push(iX + '/' + iY);
+		aSearchedCells.push(iX + '/' + iY);
+		aAttackableCells.push(iX + '/' + iY);	
 		angular.element(dCell).addClass('attackable');
 	}
 
 	function handleNeighbour(iX, iY, iMove)
 	{
-		if(oConfig.bDebug)
-			console.debug('handleNeighbour(iX='+iX+', iY='+iY+', iMove='+iMove+')');
+		if (isFoundNeighbour(iX, iY))
+			return;
 
 		var dCell	= document.querySelector('[data-x="'+iX+'"][data-y="'+iY+'"]');
 		var bUnit	= angular.element(dCell).children().attr('data-unit');
+		var iTeam1	= angular.element(dCell).children().attr('data-team');
 		var iCost	= getCost(dCell);
-console.debug(bUnit);
-		if(bUnit == 'yes')
+		var iTeam2	= oMap.troop[iSelectedTroop].team;
+
+		if(bUnit == 'true' && iTeam1!=iTeam2)
 			addAttackable(iX, iY, dCell);
 
 		iMove	-= iCost;
@@ -450,21 +191,8 @@ console.debug(bUnit);
 			return iCost;
 	}
 
-	function cleanCell()
-	{
-		var jAll	= document.querySelectorAll('.cell');
-
-		angular.element(jAll).removeClass('selected');
-		angular.element(jAll).removeClass('reachable');
-	}
-
 	function showMove()
 	{
-		if(oConfig.bDebug)
-		{
-			console.info('showMove()');
-			console.info(jSelectedTroop);
-		}
 
 		var iX		= oMap.troop[iSelectedTroop].x;
 		var iY		= oMap.troop[iSelectedTroop].y;
@@ -475,7 +203,12 @@ console.debug(bUnit);
 
 	function selectTroop()
 	{
-		cleanCell();
+		// clean prev turn marked cells
+		var jAll	= document.querySelectorAll('.cell');
+
+		angular.element(jAll).removeClass('selected');
+		angular.element(jAll).removeClass('reachable');
+		angular.element(jAll).removeClass('attackable');
 
 		// get troop coords
 		var iX		= oMap.troop[iSelectedTroop].x;
@@ -492,15 +225,67 @@ console.debug(bUnit);
 	{
 		var jSelectedDest	= angular.element(e.target);
 
+		// move troop to dest
 		if(jSelectedDest.hasClass('reachable'))
 		{
-			// move troop to dest
-			oMap.troop[iSelectedTroop].x	= jSelectedDest.attr('data-x');
-			oMap.troop[iSelectedTroop].y	= jSelectedDest.attr('data-y');
+			// get troop coords
+			var iX		= jSelectedDest.attr('data-x');
+			var iY		= jSelectedDest.attr('data-y');
+			oMap.troop[iSelectedTroop].x	= iX;
+			oMap.troop[iSelectedTroop].y	= iY;
+		
+			// Add mark on selected troop
+			var jAll	= document.querySelectorAll('.cell');
 
-			// reset cell class
-			cleanCell();
+			angular.element(jAll).removeClass('selected');
+			angular.element(jAll).removeClass('reachable');
+
+			var dCell	= document.querySelector('[data-x="'+iX+'"][data-y="'+iY+'"]');
+			angular.element(dCell).addClass('selected');
 		}
+
+		if(!aAttackableCells.length)
+		{
+			nextTroop();
+		}
+	}
+
+	function selectAtt(e)
+	{
+		var jSelectedDest	= angular.element(e.target);
+console.log(jSelectedDest);
+
+		// move troop to dest
+		if(jSelectedDest.parent().parent().hasClass('attackable'))
+		{
+console.log('attttttack');
+/*
+			// get troop coords
+			var iX		= jSelectedDest.attr('data-x');
+			var iY		= jSelectedDest.attr('data-y');
+			oMap.troop[iSelectedTroop].x	= iX;
+			oMap.troop[iSelectedTroop].y	= iY;
+		
+			// Add mark on selected troop
+			var dCell	= document.querySelector('[data-x="'+iX+'"][data-y="'+iY+'"]');
+			angular.element(dCell).addClass('selected');
+		*/
+		}
+		else
+		{
+console.log('bah merde...');
+		}
+
+		nextTroop();
+	}
+
+	function nextTroop()
+	{
+		var jAll	= document.querySelectorAll('.cell');
+
+		angular.element(jAll).removeClass('selected');
+		angular.element(jAll).removeClass('reachable');
+		angular.element(jAll).removeClass('attackable');
 
 		// next troop
 		iSelectedTroop++;
@@ -516,6 +301,7 @@ console.debug(bUnit);
 
 	// public function
 	this.selectDest		= selectDest;
+	this.selectAtt		= selectAtt;
 
 	// Init battle
 	var iTroopCount		= oMap.troop.length;
